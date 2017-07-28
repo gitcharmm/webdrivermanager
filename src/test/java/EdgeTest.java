b092676b6d3324a2959cc36deea4f81b32a2571f
gitcharmm <wgatlass@gmail.com>
2017-07-28 22:35:38 +0300

    edit class

--- src/test/java/EdgeTest.java
+++ src/test/java/EdgeTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.github.bonigarcia.wdm.EdgeDriverManager;
 
 /**
